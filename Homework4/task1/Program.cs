7b94407c871abb6eff4f7a26f159b474c10bf2dd
Wudier0 <94665879+Wudier0@users.noreply.github.com>
2022-03-25 13:42:36 +0800

    Add files via upload

---
+++ Homework4/task1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    public class Node<T>
+    {
+        public Node<T> next { get; set; }
+        public T data { get; set; }
+
+        public Node(T t)
+        {
+            next = null;
+            data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList() { tail = head = null; }
+
+        public Node<T> Head { get => head; }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+                head = tail = n;
+            else
+            {
+                tail.next = n;
+                tail = n;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var cur = head;
+            while (cur != null)
+            {
+                yield return cur.data;
+                cur = cur.next;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            foreach (var item in this)
+            {
+                action(item);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<double> list = new GenericList<double>();
+
+            Console.WriteLine("请输入链表元素个数：");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("请依次输入元素的值：");
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+
+            double max = double.MinValue;
+            double min = double.MaxValue;
+            double sum = 0;
+
+            Console.WriteLine("依次打印链表元素：");
+            list.ForEach((x) =>
+            {
+                Console.WriteLine(x);
+                max = Math.Max(max, x);
+                min = Math.Min(min, x);
+                sum += x;
+            });
+
+            Console.WriteLine($"max: {max}");
+            Console.WriteLine($"min: {min}");
+            Console.WriteLine($"sum: {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework4/task2/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+        public delegate void TickHandler(object sender, TickEventArgs args);
+
+        public class TickEventArgs
+        {
+            public DateTime Time { get; set; }
+        }
+
+        public delegate void AlarmHandler(object sender, AlarmEventArgs args);
+
+        public class AlarmEventArgs
+        {
+            public DateTime Time { get; set; }
+        }
+
+        public class Clock
+        {
+            /// <summary>
+            /// 当前时间
+            /// </summary>
+            public DateTime Time { get; set; }
+            /// <summary>
+            /// 响铃时间
+            /// </summary>
+            public DateTime AlarmTime { get; set; }
+            /// <summary>
+            /// 响铃开关
+            /// </summary>
+            public bool AlarmStatus { get; set; }
+
+            /// <summary>
+            /// 滴答事件
+            /// </summary>
+            public event TickHandler OnTick;
+            /// <summary>
+            /// 响铃事件
+            /// </summary>
+            public event AlarmHandler OnAlarm;
+
+            private bool ClockStatus;
+            private readonly TimeSpan eps = TimeSpan.FromSeconds(0.5);
+
+            public Clock()
+            {
+                Time = DateTime.MinValue;
+                AlarmTime = DateTime.MinValue;
+                ClockStatus = false;
+                AlarmStatus = false;
+                OnTick = ((c, a) => { });
+                OnAlarm = ((c, a) => { });
+            }
+
+            /// <summary>
+            /// 异步开始
+            /// </summary>
+            /// <returns></returns>
+            /// <exception cref="InvalidOperationException"></exception>
+            public async Task StartAsync()
+            {
+                if (ClockStatus)
+                    throw new InvalidOperationException("Clock already started.");
+                ClockStatus = true;
+                while (ClockStatus)
+                {
+                    Time = Time.AddSeconds(1);
+                    var args = new TickEventArgs()
+                    {
+                        Time = Time,
+                    };
+
+                    OnTick(this, args);
+
+                    if (AlarmStatus && (Time - AlarmTime).Duration() <= eps)
+                        Alarm();
+
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                }
+            }
+
+            /// <summary>
+            /// 同步非阻塞开始
+            /// </summary>
+            public void Start()
+            {
+                Task.Run(StartAsync);
+            }
+
+            /// <summary>
+            /// 结束
+            /// </summary>
+            /// <exception cref="InvalidOperationException"></exception>
+            public void Stop()
+            {
+                if (!ClockStatus)
+                    throw new InvalidOperationException("Clock already stopped.");
+                ClockStatus = false;
+            }
+
+            /// <summary>
+            /// 响铃操作
+            /// </summary>
+            private void Alarm()
+            {
+                AlarmStatus = false;
+
+                var args = new AlarmEventArgs()
+                {
+                    Time = Time,
+                };
+
+                OnAlarm(this, args);
+            }
+
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+
+            void onTick(object sender, TickEventArgs args) // 订阅滴答事件
+            {
+                Console.WriteLine($"Tick {args.Time}");
+            }
+
+            void onAlarm(object sender, AlarmEventArgs args) // 订阅响铃事件
+            {
+                Console.WriteLine($"!! Alarm {args.Time}");
+            }
+
+            clock.OnTick += onTick;
+            clock.OnAlarm += onAlarm;
+
+            // 打开时钟
+            Console.WriteLine($"Set current time and start the clock at {DateTime.Now}");
+            clock.Time = DateTime.Now;
+            clock.Start();
+            Thread.Sleep(5000);
+
+            // 关闭时钟
+            Console.WriteLine($"Stop the clock at {DateTime.Now}");
+            clock.Stop();
+
+            Thread.Sleep(2000);
+
+            // 打开时钟
+            Console.WriteLine($"Set current time and start the clock at {DateTime.Now}");
+            clock.Time = DateTime.Now;
+            clock.Start();
+
+            Thread.Sleep(1500);
+
+            // 设定响铃
+            Console.WriteLine($"Turn on the alarm and set the alarm time at {DateTime.Now + TimeSpan.FromSeconds(5)}");
+            clock.AlarmTime = DateTime.Now + TimeSpan.FromSeconds(5);
+            clock.AlarmStatus = true;
+
+            Console.ReadKey();
+        }
+    }
+}
