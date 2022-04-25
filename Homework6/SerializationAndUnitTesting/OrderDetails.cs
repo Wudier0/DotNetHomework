530c125ff32a4f032d76d83c439415b1d1028080
Wudier0 <94665879+Wudier0@users.noreply.github.com>
2022-04-25 20:10:47 +0800

    Add files via upload

---
+++ Homework6/SerializationAndUnitTesting/OrderDetails.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem
+{
+    public class OrderDetails
+    {
+        public Goods Goods { get; set; }
+        public int Amount { get; set; }
+
+        public float GoodsTotalPrice
+        {
+            get => Goods.Price * Amount;
+        }
+
+        public OrderDetails(Goods goods,int amount)
+        {
+            Goods = goods;
+            Amount = amount;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var details = obj as OrderDetails;
+            return details != null && Goods == details.Goods;
+        }
+
+        public override int GetHashCode()
+        {
+            return Goods.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"goods:{Goods}, amount:{Amount}, goodsTotalPrice:{GoodsTotalPrice}";
+        }
+    }
+}
